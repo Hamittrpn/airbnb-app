0f100074b5382303116ca98e7f56bc1438e8494c
Hamit Tırpan <hamit.tirpan@logiwa.com>
2024-02-10 12:46:53 +0300

    Initial Commit

---
+++ AirbnbApp/AirbnbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbAppApp.swift
+//  AirbnbApp
+//
+//  Created by Hamit Tırpan on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
